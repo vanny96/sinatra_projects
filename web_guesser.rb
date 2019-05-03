35ad0612cccb13f406b5a18df2aba97b8698568e
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-05-03 10:00:35 +0200

    I3: guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,10 +2,32 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+def guess_check number
+  case number 
+  when RANDOM_NUMBER 
+    return "CORRECT!"
+  when RANDOM_NUMBER..RANDOM_NUMBER+5
+    return "Too high!"
+  when RANDOM_NUMBER-5..RANDOM_NUMBER 
+    return "Too low!"
+  when 0..RANDOM_NUMBER-5
+    return "Way Too low!"
+  when RANDOM_NUMBER+5..100
+    return "Way Too high!"
+  end
+end
 
-random_number = rand(101)
+RANDOM_NUMBER = rand(101)
 
 get '/' do
-  erb :index, :locals => {:number => random_number}
+
+  result = ""
+
+  unless params['guess'].nil?
+    guess = params['guess'].to_i
+    result= guess_check guess
+  end
+
+  erb :index, :locals => {:result => result}
 end
 
