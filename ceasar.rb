197c78a25e1efe1ccf995aa1bf501ebc3267ad84
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-05-03 10:45:55 +0200

    Added index

--- ceasar.rb
+++ ceasar.rb
@@ -4,8 +4,12 @@ require "sinatra/reloader"
 require_relative "ceasar-cipher"
 
 get "/" do
+  erb :index
+end
+
+get "/caesar" do
   unless params["line"].nil?
     new_line = caesar_cipher params["line"], params["number"].to_i
   end
-  erb :index, :locals => {:new_line => new_line}
+  erb :caesar, :locals => {:new_line => new_line}
 end
